c4455c324dbfe1199cc9fcdec57407be1693bd51
damon holden 612924336 <damon.holden@bt.com>
2023-10-28 15:14:21 +0100

    feat: add multiple get endpoints

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ import http from 'node:http';
 
 const server = http.createServer();
 
-server.on('request', (request, response) => {
-	response.end('Hello, World!');
-});
+server
+	.on('request', (request, response) => {
+		const { method, url } = request;
 
-server.listen(8000, () => {
-	console.log('Node server created at port 8000');
-});
+		if (method === 'GET' && url === '/') {
+			response.writeHead(200, { 'Content-Type': 'text/html' });
+			response.write('<html><a href="/account">Account</a></html>');
+			response.end();
+		}
+
+		if (method === 'GET' && url === '/account') {
+			response.writeHead(200, { 'Content-Type': 'text/plain' });
+			response.write("Jimmy's user data");
+			response.write('\n');
+			response.write("Sandra's user data");
+			response.end();
+		}
+	})
+	.listen(8000, () => {
+		console.log('Node server created at port 8000');
+	});
